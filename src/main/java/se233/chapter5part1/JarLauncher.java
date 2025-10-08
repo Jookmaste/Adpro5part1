862f3c63ecbdf61f30793c2cc807c09669ffb7df
ไอ่ปุณณ์ <Punnlaowatchara@gmail.com>
2025-10-08 23:18:52 +0700

    chapter 6

---
+++ src/main/java/se233/chapter5part1/JarLauncher.java
@@ -0,0 +1,11 @@
+package se233.chapter5part1;
+
+// ต้อง import คลาส Launcher จาก view package
+import se233.chapter5part1.Launcher;
+
+public class JarLauncher {
+    public static void main(String[] args) {
+        // เรียกใช้เมธอด main() ของคลาส JavaFX Launcher
+        Launcher.main(args);
+    }
+}
